bd2f737abba2d605580c36bd5090f62f96c782fa
Super Dev <super.dev.947@gmail.com>
2025-07-17 13:38:45 -0400

    add

--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import * as React from 'react'
+import { colorSchemes, shadows, shape, typography } from './themePrimitives'
 
 interface AppThemeProps {
   children: React.ReactNode
@@ -8,7 +9,16 @@ interface AppThemeProps {
 export default function AppTheme(props: AppThemeProps) {
   const { children } = props
   const theme = React.useMemo(() => {
-    return createTheme()
+    return createTheme({
+      cssVariables: {
+        colorSchemeSelector: 'data-mui-color-scheme',
+        cssVarPrefix: 'template'
+      },
+      colorSchemes,
+      typography,
+      shadows,
+      shape
+    })
   }, [])
 
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>
